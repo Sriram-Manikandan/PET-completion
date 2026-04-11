49b40d76c10c6afb109b745812fd81586dbfe28c
Sriram-Manikandan <sriramtech2007@gmail.com>
2026-04-11 15:43:45 +0530

    added milestone 1 challenge 7

--- milestone-1-challenge-4/vibe-version/b_X87eDJqFnwo/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TaskManager } from "@/components/task-manager"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background py-12 px-4">
-      <TaskManager />
-    </main>
-  )
-}
